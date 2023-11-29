f5e64bb3500ec18f7640ddc356beb6b9ec5aab7e
Quân Phạm <89670950+CrowsDenDui@users.noreply.github.com>
2023-11-29 23:17:08 +0700

    cứu tui dới

---
+++ Đồ án/Code/DA.py
@@ -0,0 +1,317 @@
+import numpy as np                               
+import pandas as pd                               
+import seaborn as sns
+import datetime
+from datetime import date
+import plotly.graph_objects as go
+import matplotlib.pyplot as plt
+from sklearn.preprocessing import LabelEncoder
+from sklearn import preprocessing 
+from math import sqrt
+from sklearn.cluster import KMeans
+from sklearn.metrics import silhouette_score
+from apyori import apriori
+from mlxtend.frequent_patterns import apriori
+from mlxtend.frequent_patterns import association_rules
+from sklearn.linear_model import LogisticRegression
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+from sklearn.metrics import classification_report
+from sklearn.metrics import accuracy_score,confusion_matrix,f1_score
+import warnings
+warnings.filterwarnings("ignore", category=UserWarning, module="matplotlib")
+warnings.filterwarnings('ignore')
+
+df = pd.read_csv('Doan\marketing_campaign.csv',header=0, sep="\t")
+
+df = df.drop(['Z_CostContact', 'Z_Revenue'],axis=1)
+
+df['Income'] = df['Income'].fillna(df['Income'].mean())
+df.isna().any()
+
+df['Marital_Status'] = df['Marital_Status'].replace(['Married', 'Together'],'relationship')
+df['Marital_Status'] = df['Marital_Status'].replace(['Divorced', 'Widow', 'Alone', 'YOLO', 'Absurd'],'Single')
+
+product_data = []
+for i in range(0, len(df)):
+  productdata = [df['MntWines'][i], df['MntFruits'][i], 
+                  df['MntMeatProducts'][i], df['MntFishProducts'][i], 
+                  df['MntSweetProducts'][i], df['MntGoldProds'][i]]
+  product_data.append(productdata)
+Products_DF = pd.DataFrame(product_data, columns = ['Wines', 'Fruits', 'Meat', 'Fish', 'Sweets', 'Gold'])
+Products_DF.head()
+
+df['Kids'] = df['Kidhome'] + df['Teenhome']
+df['Expenses'] = df['MntWines'] + df['MntFruits'] + df['MntMeatProducts'] + df['MntFishProducts'] + df['MntSweetProducts'] + df['MntGoldProds']
+df['TotalAcceptedCmp'] = df['AcceptedCmp1'] + df['AcceptedCmp2'] + df['AcceptedCmp3'] + df['AcceptedCmp4'] + df['AcceptedCmp5'] + df['Response']
+df['NumTotalPurchases'] = df['NumWebPurchases'] + df['NumCatalogPurchases'] + df['NumStorePurchases'] + df['NumDealsPurchases']
+
+col_del = ["AcceptedCmp1" , "AcceptedCmp2", "AcceptedCmp3" , "AcceptedCmp4","AcceptedCmp5", "Response","NumWebVisitsMonth", "NumWebPurchases","NumCatalogPurchases","NumStorePurchases","NumDealsPurchases" , "Kidhome", "Teenhome","MntWines", "MntFruits", "MntMeatProducts", "MntFishProducts", "MntSweetProducts", "MntGoldProds"]
+df=df.drop(columns=col_del,axis=1)
+
+df['Age'] = 2015 - df["Year_Birth"]
+
+df['Education'] = df['Education'].replace(['PhD','2n Cycle','Graduation', 'Master'],'PG')  
+df['Education'] = df['Education'].replace(['Basic'], 'UG')
+
+df['Dt_Customer'] = pd.to_datetime(df.Dt_Customer, format = '%d-%m-%Y')
+df['first_day'] = '01-01-2015'
+df['first_day'] = pd.to_datetime(df.first_day)
+df['day_engaged'] = (df['first_day'] - df['Dt_Customer']).dt.days
+
+df=df.drop(columns=["ID", "Dt_Customer", "first_day", "Year_Birth", "Dt_Customer", "Recency", "Complain"],axis=1)
+
+print(df.head())
+
+plt.rcParams.update(plt.rcParamsDefault)
+
+plt.figure(figsize=(8,8))
+sns.barplot(x=df['Marital_Status'], y=df['Expenses'], hue = df["Education"])
+plt.title("Analysis of the Correlation between Marital Status and Expenses with respect to Education")
+
+plt.figure(figsize=(8,8))
+sns.barplot(x=df['Marital_Status'], y=df['Expenses'])
+plt.title("Analysis of the Correlation between Marital Status and Expenses")
+
+plt.figure(figsize=(8,8))
+plt.hist("Expenses", data = df[df["Marital_Status"] == "relationship"], alpha = 0.5, label = "relationship")
+plt.hist("Expenses", data = df[df["Marital_Status"] == "Single"], alpha = 0.5, label = "Single")
+plt.title("Distribution of Expenses with respect to Marital Status")
+plt.xlabel("Expenses")
+plt.legend(title = "Marital Status")
+
+plt.figure(figsize=(8,8))
+plt.hist("Expenses", data = df[df["Education"] == "PG"], alpha = 0.5, label = "PG")
+plt.hist("Expenses", data = df[df["Education"] == "UG"], alpha = 0.5, label = "UG")
+plt.title("Distribution of Expenses with respect to Education")
+plt.xlabel("Expenses")
+plt.legend(title = "Education")
+
+plt.figure(figsize=(8,8))
+plt.hist("NumTotalPurchases", data = df[df["Education"] == "PG"], alpha = 0.5, label = "PG")
+plt.hist("NumTotalPurchases", data = df[df["Education"] == "UG"], alpha = 0.5, label = "UG")
+plt.title("Distribution of Number of Total Purchases with respect to Education")
+plt.xlabel("Number of Total Purchases")
+plt.legend(title = "Education")
+
+plt.figure(figsize=(8,8))
+plt.hist("Age", data = df[df["Marital_Status"] == "relationship"], alpha = 0.5, label = "relationship")
+plt.hist("Age", data = df[df["Marital_Status"] == "Single"], alpha = 0.5, label = "Single")
+plt.title("Distribution of Age with respect to Marital Status")
+plt.xlabel("Age")
+plt.legend(title = "Marital Status")
+
+plt.figure(figsize=(8,8))
+plt.hist("Income", data = df[df["Marital_Status"] == "relationship"], alpha = 0.5, label = "relationship")
+plt.hist("Income", data = df[df["Marital_Status"] == "Single"], alpha = 0.5, label = "Single")
+plt.title("Distribution of Income with respect to Marital Status")
+plt.xlabel("Income")
+plt.legend(title = "Marital Status")
+
+plt.figure(figsize=(8,8))
+plt.pie(df["Marital_Status"].value_counts(), labels = ["relationship", "Single"], autopct='%1.1f%%', counterclock=False)
+plt.legend()
+
+plt.figure(figsize=(8,8))
+plt.pie(df["Education"].value_counts(), labels = ["PG", "UG"], autopct='%1.1f%%', counterclock=False)
+plt.legend()
+
+sns.barplot(x = df['Expenses'],y = df['Education']);
+plt.title('Total Expense based on the Education Level');
+
+sns.barplot(x = df['Income'],y = df['Education']);
+plt.title('Total Income based on the Education Level');
+
+plt.show()
+
+cate = []
+for i in df.columns:
+    if (df[i].dtypes == "object"):
+        cate.append(i)
+
+print(cate)
+
+lbl_encode = LabelEncoder()
+for i in cate:
+    df[i]=df[[i]].apply(lbl_encode.fit_transform)
+
+df1 = df.copy()
+
+plt.figure(figsize=(5,5))
+ax = sns.boxplot(data=df1 , orient="h")
+plt.title('A boxplot: Outliers in the dataset', color = 'blue')
+plt.xlabel('Count/ Frequency')
+plt.show()
+
+q3 = df1.quantile(0.75)
+q1 = df1.quantile(0.25)
+iqr = q3-q1
+lower_range = q1 - (1.5 * iqr)
+upper_range = q3 + (1.5 * iqr)
+
+df1 = df1[~( (df1 < lower_range)|(df1 > upper_range) ).any(axis=1)]
+
+wcss=[] ##Initializing the list for the values of WCSS  
+for i in range (1,11): #for diferent values of k ranging from 1 to 10
+ kmeans=KMeans(n_clusters=i,init='k-means++',random_state=42)
+ kmeans.fit(df1)
+ wcss.append(kmeans.inertia_)
+plt.figure(figsize=(16,8))
+plt.plot(range(1,11),wcss, 'bx-')
+plt.title('The Elbow Method')
+plt.xlabel('Number of clusters')
+plt.ylabel('WCSS')
+plt.show()
+
+silhouette_scores = []
+for i in range(2,10):
+    m1=KMeans(n_clusters=i, random_state=42)
+    c = m1.fit_predict(df1)
+    silhouette_scores.append(silhouette_score(df1, m1.fit_predict(df1))) 
+plt.bar(range(2,10), silhouette_scores) 
+plt.xlabel('Number of clusters', fontsize = 20) 
+plt.ylabel('S(i)', fontsize = 20) 
+plt.show()
+
+sc=max(silhouette_scores)
+number_of_clusters=silhouette_scores.index(sc)+2
+print("Number of Cluster Required is : ", number_of_clusters)
+
+kmeans=KMeans(n_clusters=number_of_clusters, random_state=42).fit(df1)
+pred=kmeans.predict(df1)
+
+df1['cluster'] = pred + 1
+
+pl = sns.countplot(x=df1["cluster"])
+pl.set_title("Distribution Of The Clusters")
+plt.show()
+
+sns.set(rc={'axes.facecolor':'black', 'figure.facecolor':'black', 'axes.grid' : False, 'font.family': 'Ubuntu'})
+
+for i in df1:
+    diag = sns.FacetGrid(df1, col = "cluster", hue = "cluster", palette = "Set1")
+    diag.map(plt.hist, i, bins=6, ec="k") 
+    diag.set_xticklabels(rotation=25, color = 'white')
+    diag.set_yticklabels(color = 'white')
+    diag.set_xlabels(size=16, color = 'white')
+    diag.set_titles(size=16, color = '#f01132', fontweight="bold")
+    diag.fig.set_figheight(6)
+
+data = df1.copy()
+
+cut_labels_Age = ['Young', 'Adult', 'Mature', 'Senior']
+cut_bins = [0, 30, 45, 65, 120]
+data['Age_group'] = pd.cut(data['Age'], bins=cut_bins, labels=cut_labels_Age)
+
+cut_labels_Income = ['Low income', 'Low to medium income', 'Medium to high income', 'High income']
+data['Income_group'] = pd.qcut(data['Income'], q=4, labels=cut_labels_Income)
+
+cut_labels_dayengaged = ['New customers', 'Discovering customers', 'Experienced customers', 'Old customers']
+data['dayengaged_group'] = pd.qcut(data['day_engaged'], q=4, labels=cut_labels_dayengaged)
+data=data.drop(columns=['Age','Income','day_engaged'])
+
+cut_labels = ['Least Active Customer', 'Highly Active Customer']
+data['Wines_segment'] = pd.qcut(Products_DF['Wines'][Products_DF['Wines']>0],q=[0, 0.5 ,1], labels=cut_labels).astype("object")
+data['Fruits_segment'] = pd.qcut(Products_DF['Fruits'][Products_DF['Fruits']>0],q=[0, 0.5, 1], labels=cut_labels).astype("object")
+data['Meat_segment'] = pd.qcut(Products_DF['Meat'][Products_DF['Meat']>0],q=[0, 0.5,1], labels=cut_labels).astype("object")
+data['Fish_segment'] = pd.qcut(Products_DF['Fish'][Products_DF['Fish']>0],q=[0, 0.5, 1], labels=cut_labels).astype("object")
+data['Sweets_segment'] = pd.qcut(Products_DF['Sweets'][Products_DF['Sweets']>0],q=[0, 0.5, 1], labels=cut_labels).astype("object")
+data['Gold_segment'] = pd.qcut(Products_DF['Gold'][Products_DF['Gold']>0],q=[0, 0.5, 1], labels=cut_labels).astype("object")
+data.replace(np.nan, "Inactive Customer",inplace=True)
+data = data.astype(object)
+
+data.head()
+
+pd.set_option('display.max_columns', None)
+pd.set_option('display.max_rows', None)
+pd.set_option('display.max_colwidth', 999)
+pd.options.display.float_format = "{:.3f}".format
+
+association = data.copy() 
+association.head()
+
+association.drop(["Education", "Marital_Status", "Kids", "Expenses", "TotalAcceptedCmp", "NumTotalPurchases", "cluster"], axis = 1, inplace = True)
+association.head()
+
+df_ap = pd.get_dummies(association)
+min_support = 0.08
+max_len = 10
+frequent_items = apriori(df_ap, use_colnames=True, min_support=min_support, max_len=max_len + 1)
+rules = association_rules(frequent_items, metric='lift', min_threshold=1)
+
+product='Wines'
+segment='Highly Active Customer'
+target = '{\'%s_segment_%s\'}' %(product,segment)
+results_personnel_care = rules[rules['consequents'].astype(str).str.contains(target, na=False)].sort_values(by='confidence', ascending=False)
+results_personnel_care.head()
+
+product='Fruits'
+segment='Highly Active Customer'
+target = '{\'%s_segment_%s\'}' %(product,segment)
+results_personnel_care = rules[rules['consequents'].astype(str).str.contains(target, na=False)].sort_values(by='confidence', ascending=False)
+results_personnel_care.head()
+
+product='Meat'
+segment='Highly Active Customer'
+target = '{\'%s_segment_%s\'}' %(product,segment)
+results_personnel_care = rules[rules['consequents'].astype(str).str.contains(target, na=False)].sort_values(by='confidence', ascending=False)
+results_personnel_care.head()
+
+product='Fish'
+segment='Highly Active Customer'
+target = '{\'%s_segment_%s\'}' %(product,segment)
+results_personnel_care = rules[rules['consequents'].astype(str).str.contains(target, na=False)].sort_values(by='confidence', ascending=False)
+results_personnel_care.head()
+
+product='Sweets'
+segment='Highly Active Customer'
+target = '{\'%s_segment_%s\'}' %(product,segment)
+results_personnel_care = rules[rules['consequents'].astype(str).str.contains(target, na=False)].sort_values(by='confidence', ascending=False)
+results_personnel_care.head()
+
+product='Gold'
+segment='Highly Active Customer'
+target = '{\'%s_segment_%s\'}' %(product,segment)
+results_personnel_care = rules[rules['consequents'].astype(str).str.contains(target, na=False)].sort_values(by='confidence', ascending=False)
+results_personnel_care.head()
+
+df2=df1.copy()
+x = df2.drop('cluster', axis=1)
+y = df2['cluster']
+
+x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2, random_state=2)
+
+sc = StandardScaler()
+x_train = sc.fit_transform(x_train)
+x_test = sc.transform(x_test)
+
+log_reg = LogisticRegression()
+log_reg.fit(x_train, y_train)
+
+y_predicted = log_reg.predict(x_test)
+
+print("Classification Report: \n", classification_report(y_test,y_predicted))
+print("-" * 100)
+print()
+    
+acc = accuracy_score(y_test, y_predicted)
+
+print("Accuracy Score: ", acc)
+print("-" * 100)
+print()
+
+f1 = f1_score(y_test, y_predicted)
+
+print("F1 Score: ", f1)
+print("-" * 100)
+print()
+    
+print("Confusion Matrix: ")
+plt.figure(figsize=(10, 5))
+sns.heatmap(confusion_matrix(y_test, y_predicted), annot=True, fmt='g');
+plt.title('Confusion Matrix', fontsize=20)
+plt.show()
+
+# sns.countplot(df.Marital_Status)
+# sns.set(rc={'figure.figsize':(4,4)})
+# plt.show()
